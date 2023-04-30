7940776db6585c1933a81f80fc3cce072389ae04
Stephen Nwaukwa <interprimos80@hotmail.com>
2023-04-29 23:34:58 -0500

    put webdriver in fxn; create fxn to scrape data

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 # from webdriver_manager.chrome import ChromeDriverManager
 
-ser = Service(r"C:\Development\\chromedriver.exe")
 
-options = webdriver.ChromeOptions()
-options.add_argument("disable-infobars")
-options.add_argument("start-maximized")
-options.add_argument("disable-dev-shm-usage")
-options.add_argument("no-sandbox")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_argument("disable-blink-features=AutomationControlled")
+def get_driver():
+    ser = Service(r"C:\Development\\chromedriver.exe")
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(service=ser, options=options)
+
+    driver.get("http://automated.pythonanywhere.com/")
+    return driver
 
 
-driver = webdriver.Chrome(service=ser, options=options)
+def main():
+    driver = get_driver()
+    element = driver.find_element(
+        by="xpath", value="/html/body/div[1]/div/h1[1]")
+    return element.text
 
 
-driver.get("https://www.google.com")
+print(main())
