a26244e6fb7fc30b765bcafc693f6e5beb5f5c01
Stephen Nwaukwa <interprimos80@hotmail.com>
2023-04-29 23:13:00 -0500

    add webdriver options

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from selenium.webdriver.chrome.service import Service
 # from webdriver_manager.chrome import ChromeDriverManager
 
 ser = Service(r"C:\Development\\chromedriver.exe")
-op = webdriver.ChromeOptions()
 
-driver = webdriver.Chrome(service=ser, options=op)
+options = webdriver.ChromeOptions()
+options.add_argument("disable-infobars")
+options.add_argument("start-maximized")
+options.add_argument("disable-dev-shm-usage")
+options.add_argument("no-sandbox")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_argument("disable-blink-features=AutomationControlled")
+
+
+driver = webdriver.Chrome(service=ser, options=options)
 
 
 driver.get("https://www.google.com")
-# driver.close()
